bff86ac23fe20c7b3dc9f82570aea9effadd6814
alexandre.gabriel <alexandregabriel_@hotmail.com>
2022-01-14 16:31:10 -0300

    

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,19 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
-import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    // TESTE
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+            
+        })
+
     return (
         <>
-
             <Pagination />
 
             <div className="container">
@@ -27,10 +35,7 @@ function Listing() {
                     </div>
                 </div>
             </div>
-            
-            <MovieScore />
-        </>
-        
+        </>  
     )
 }
 
